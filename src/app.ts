96f8becad145f22c7240cc491207d64d8744f796
Marko Pavlovic <markopavlovic205051@gmail.com>
2023-05-22 19:30:01 +0200

    feat: add discord client and events

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,13 @@ import { dirname, join } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import prismaPlugin from "./plugins/prisma";
+import discordPlugin from "./plugins/discord";
 import { swaggerOpts, swaggerUiOpts } from "./utils/swagger";
 
 export function createServer(opts: FastifyServerOptions = {}): FastifyInstance {
     const app = fastify(opts).withTypeProvider<TypeBoxTypeProvider>();
 
-    app.register(prismaPlugin);
+    app.register(discordPlugin);
 
     app.register(swagger, swaggerOpts);
     app.register(swaggerUI, swaggerUiOpts);
---
+++ src/plugins/discord.ts
@@ -0,0 +1,38 @@
+import fp from "fastify-plugin";
+import { FastifyPluginAsync } from "fastify";
+import { Client, Events, GatewayIntentBits, IntentsBitField } from "discord.js";
+import "dotenv/config";
+import { Config } from "../config";
+
+// Use TypeScript module augmentation to declare the type of server.prisma to be PrismaClient
+declare module "fastify" {
+    interface FastifyInstance {
+        discord: Client;
+    }
+}
+
+const discordPlugin: FastifyPluginAsync = fp(async server => {
+    const disocrd = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.DirectMessages,
+            IntentsBitField.Flags.DirectMessages,
+        ],
+    });
+
+    disocrd.once(Events.ClientReady, c => {
+        server.log.info(`Ready! Logged in as ${c.user.tag}`);
+    });
+
+    disocrd.on(Events.GuildMemberAdd, async member => {
+        await member.send("Welcome to the server! Enjoy your stay.");
+    });
+    disocrd.login(Config.token);
+
+    // Make Discord Client available through the fastify server instance: server.discord
+    server.decorate("discord", disocrd);
+});
+export default discordPlugin;
